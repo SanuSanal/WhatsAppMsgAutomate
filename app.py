48189120b311b1d8a8c0b095c5b0446c057193b7
SanuSanal <=>
2022-07-21 22:45:54 +0530

    handling error scenarios 1. image not found 2. image is not added in the excel sheet

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from types import NoneType
+
 import openpyxl as xl
 from io import BytesIO
 import time
@@ -20,17 +22,27 @@ def send_to_clipboard(filepath):
     win32clipboard.CloseClipboard()
 
 
+image_folder = 'imgs\\'
+
 wb = xl.load_workbook('contacts.xlsx')
 sheet = wb['Sheet1']
 
 for row in range(2, sheet.max_row + 1):
+    image_exists = False
+    image_file_path = 'No image added'
     mobile_number = sheet.cell(row, 1).value
     message = sheet.cell(row, 2).value
-    image_file_path = 'imgs\\' + sheet.cell(row, 3).value
+    try:
+        if type(sheet.cell(row, 3).value) is not NoneType:
+            image_exists = True
+            image_file_path = image_folder + sheet.cell(row, 3).value
+            send_to_clipboard(image_file_path)
+    except FileNotFoundError:
+        image_file_path = 'No images found!'
     print(f'sending message, To: {mobile_number}, message: {message}, image location: {image_file_path}')
     webbrowser.open(f'whatsapp://send?phone={mobile_number}&text={message}')
-    send_to_clipboard(image_file_path)
     time.sleep(1)
-    press_and_release('ctrl+v')
-    time.sleep(1)
+    if image_exists:
+        press_and_release('ctrl+v')
+        time.sleep(1)
     press_and_release('enter')
